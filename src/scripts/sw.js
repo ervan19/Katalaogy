d512fa1aa87574b99c247ca1beb060175dcaf87e
ervan19 <ervanmuhammada454@gmail.com>
2022-11-01 21:13:49 +0700

    Configuration sw.js

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,48 +1,9 @@
-<<<<<<< HEAD
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-restricted-globals */
-=======
->>>>>>> 67b1175 (Configuration sw.js)
 import regeneratorRuntime from "regenerator-runtime";
 import { registerRoute } from "workbox-routing";
 import { precacheAndRoute } from "workbox-precaching";
 import { ExpirationPlugin } from "workbox-expiration";
-<<<<<<< HEAD
-import { CacheFirst, NetworkFirst } from "workbox-strategies";
-import { skipWaiting, clientsClaim, setCacheNameDetails } from "workbox-core";
+import {  NetworkFirst } from "workbox-strategies";
 
-skipWaiting();
-clientsClaim();
-
-setCacheNameDetails({
-  prefix: "comfy-resto",
-  precache: "precache",
-});
-
-precacheAndRoute(self.__WB_MANIFEST);
-
-registerRoute(
-  /^https:\/\/restaurant-api\.dicoding\.dev\//,
-  new NetworkFirst({
-    cacheName: "restaurant-api",
-    plugins: [
-      new ExpirationPlugin({
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-      }),
-    ],
-  })
-);
-
-registerRoute(
-  /\.(?:png|jpx|css|svg)$/,
-  new CacheFirst({
-    cacheName: "comfy-resto-images",
-    plugins: [
-      new ExpirationPlugin({
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-        maxEntries: 30,
-=======
-import { NetworkFirst } from "workbox-strategies";
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -60,7 +21,6 @@ registerRoute(
       new ExpirationPlugin({
         maxAgeSeconds: 60 * 60 * 24 * 30,
         maxEntries: 100,
->>>>>>> 67b1175 (Configuration sw.js)
       }),
     ],
   })
