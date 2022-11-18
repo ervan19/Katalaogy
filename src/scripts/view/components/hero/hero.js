9f051fbb6ce046c9bcee81834e4330f9367a0495
ervan19 <ervanmuhammada454@gmail.com>
2022-11-18 11:24:08 +0700

    fixed background image hero

--- src/scripts/view/components/hero/hero.js
+++ src/scripts/view/components/hero/hero.js
@@ -1,15 +1,15 @@
 class Hero extends HTMLElement {
-  connectedCallback() {
-    this.render();
-  }
+    connectedCallback() {
+        this.render();
+    }
 
-  render() {
-    this.innerHTML = `
+    render() {
+        this.innerHTML = `
         <div class="hero" tabindex="0" aria-label="Hero Image ">
             <h1 tabindex="0">Find your Favorite <br><span>Restaurant catalog</span> in Katalaogy</h1>
         </div>
     `;
-  }
+    }
 }
 
 customElements.define("hero-element", Hero);
